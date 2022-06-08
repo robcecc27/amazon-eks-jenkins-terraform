03b2e6e89580f3848ee6ec0392c0cd993dde0e06
robcecc27 <rob.cecchini@gmail.com>
2022-06-07 19:02:29 -0500

    initial commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 
 provider "aws" {
-  region                  = "${var.region}"
-  # shared_credentials_file = "~/.aws/credentials"
-  # profile                 = "development"
+  region                   = var.region
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "default"
 }
-
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  default = "us-east-1"
+  default = "default"
 }
 variable "environment" {
   default = "Development"
@@ -32,5 +32,5 @@ variable "instance_ami" {
 
 }
 variable "keyname" {
-  
+
 }
